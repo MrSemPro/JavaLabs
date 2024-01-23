f4d43cd016f493decb6ede6e0aadb723b4a5afc7
MrSemPro <psm3stars@gmail.com>
2024-01-23 09:10:50 +0300

    lab3

---
+++ Лабораторные/Lab3/src/Strategy/StrategyService.java
@@ -0,0 +1,10 @@
+package Strategy;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+public interface StrategyService {
+    String exec(List<String> strings);
+
+    StrategyService setStrategy(@NotNull Strategy strategy);
+}
