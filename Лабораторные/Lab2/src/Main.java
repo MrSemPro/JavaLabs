5d22c75141c50f93bc888ca25a63284a04115d1e
MrSemPro <psm3stars@gmail.com>
2024-01-23 05:58:05 +0300

    Bridge

--- Лабораторные/Lab2/src/Main.java
+++ Лабораторные/Lab2/src/Main.java
@@ -1,9 +1,11 @@
 import Adapter.AdapterService;
-
+import Bridge.BridgeService;
 public class Main {
     public static void main(String[] args) {
         AdapterService adapterService = new AdapterService();
         adapterService.exec();
 
+        BridgeService bridgeService = new BridgeService();
+        bridgeService.exec("Radio");
     }
 }
