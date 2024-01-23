69a86870fa9c27638ddd68396d0d2dd6436dfaab
MrSemPro <psm3stars@gmail.com>
2024-01-23 04:57:05 +0300

    Singleton

---
+++ Лабораторные/Lab1/src/Singleton/Singleton.java
@@ -0,0 +1,11 @@
+package Singleton;
+
+public class Singleton {
+    public void exec() {
+        Connection connection1 = Connection.getInstance();
+        Connection connection2 = Connection.getInstance();
+
+        System.out.println(connection1.toString());
+        System.out.println(connection2.toString());
+    }
+}
